d8f35e06b39e40dbf5ac549fdb1e05cf85410fd6
Felix <felix.lonergan@gmail.com>
2020-04-13 23:34:39 +1000

    Black formatting (no functionality change)

--- SGD_tfidf.py
+++ SGD_tfidf.py
@@ -7,8 +7,9 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.4.2
 #   kernelspec:
-#     display_name: 'Python 3.6.5 64-bit (''Jose Ca'': virtualenv)'
-#     name: python36564bitjosecavirtualenvd7d530439b5748e4aeb4afcd92dcbe4b
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 import matplotlib.pyplot as plt
@@ -16,52 +17,64 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer,TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report,confusion_matrix
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix,
+)
 from sklearn.pipeline import Pipeline
+
 sns.set()
 
-train = pd.read_csv('training.csv')
-plt.figure(figsize=(10,3))
+train = pd.read_csv("training.csv")
+plt.figure(figsize=(10, 3))
 plt.legend()
 # distribution of classes
-train['topic'].value_counts().plot(kind='barh',color='green') 
+train["topic"].value_counts().plot(kind="barh", color="green")
 
-topics = sorted(train['topic'].value_counts().keys().values.tolist())
-labels = [[label,category] for label,category in enumerate(topics)]
-labels # for reference only when encoding
+topics = sorted(train["topic"].value_counts().keys().values.tolist())
+labels = [[label, category] for label, category in enumerate(topics)]
+labels  # for reference only when encoding
 
 from sklearn import preprocessing
+
 """used to convert categorical to id for predicting documents. sklearn trains different binary models based on id of target """
-le = preprocessing.LabelEncoder() 
-le.fit(train['topic'])
+le = preprocessing.LabelEncoder()
+le.fit(train["topic"])
 classes = list(le.classes_)
 list(enumerate(le.classes_))
 
-sgd_f = Pipeline([('vect', CountVectorizer()),
-               ('tfidf', TfidfTransformer()),
-               ('clf', SGDClassifier()),
-              ])
+sgd_f = Pipeline(
+    [
+        ("vect", CountVectorizer()),
+        ("tfidf", TfidfTransformer()),
+        ("clf", SGDClassifier()),
+    ]
+)
 
-train['target'] = le.transform(train['topic'])
-#Split data
-X_train,X_test_dev = train.iloc[:9000,:-1],train.iloc[9000:,:-1]
-y_train, y_test_dev = train.iloc[:9000,-1],train.iloc[9000:,-1]
-sgd_f.fit(X_train['article_words'],y_train)
-y_pred = sgd_f.predict(X_test_dev['article_words'])
-print(f'accuracy {accuracy_score(y_test_dev,y_pred)}')
-print(classification_report(y_test_dev, y_pred,target_names=classes))
+train["target"] = le.transform(train["topic"])
+# Split data
+X_train, X_test_dev = train.iloc[:9000, :-1], train.iloc[9000:, :-1]
+y_train, y_test_dev = train.iloc[:9000, -1], train.iloc[9000:, -1]
+sgd_f.fit(X_train["article_words"], y_train)
+y_pred = sgd_f.predict(X_test_dev["article_words"])
+print(f"accuracy {accuracy_score(y_test_dev,y_pred)}")
+print(classification_report(y_test_dev, y_pred, target_names=classes))
 
-confusion_matrix(y_test_dev,y_pred)
+confusion_matrix(y_test_dev, y_pred)
 
-test = pd.read_csv('test.csv')
-y_test = le.transform(test['topic'])
-X_test = test['article_words']
+test = pd.read_csv("test.csv")
+y_test = le.transform(test["topic"])
+X_test = test["article_words"]
 y_pred = sgd_f.predict(X_test)
-print(f'accuracy {accuracy_score(y_test,y_pred)}')
-print(classification_report(y_test, y_pred,target_names=classes))
+print(f"accuracy {accuracy_score(y_test,y_pred)}")
+print(classification_report(y_test, y_pred, target_names=classes))
 
-confusion_matrix(y_test,y_pred)
+confusion_matrix(y_test, y_pred)
 
-
+sns.heatmap(confusion_matrix(y_test, y_pred))
